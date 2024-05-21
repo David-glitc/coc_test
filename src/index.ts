d3a9797f2718825ae6381e85352609d0ac9f3bd6
David_glitc <davidopuene8@gmail.com>
2024-05-21 12:36:28 +0100

    re test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import {Sdk} from "@unique-nft/sdk"
-import {Sr25519Account} from "@unique-nft/sr25519"
+import { Sdk } from "@unique-nft/sdk"
+import { Account, Sr25519Account } from "@unique-nft/sr25519"
 import { NFT_config } from "./config"
+import { EventEmitter } from "prompt"
 
 
-if (NFT_config.ownerSeed) {
-    const account = Sr25519Account.fromUri(NFT_config.ownerSeed)
 
-    console.log(account.address)
-    
+function main() {
+
+    if (NFT_config.ownerSeed) {
+        const account = Sr25519Account.fromUri(NFT_config.ownerSeed)
+        console.log(account.address)        
+    } else {
+        throw new Error("No user Seed Phrase set");
+    }
+
+
 }
+
+main()
